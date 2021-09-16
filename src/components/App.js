0f5c2d77514b186de5860e179ea9606a04ece5a2
sepaev <sepaev@gmail.com>
2021-09-16 21:20:52 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,33 @@
-import { Component, useState } from 'react';
+import { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions';
 import Section from './Section';
 import Statistics from './Statistics';
 
 function App() {
-  const [good, setGood] = useState('Good');
-  const [bad, setBad] = useState('Bad');
-  const [neutral, setNeutral] = useState('Neutral');
-  const [state] = useState();
-
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  console.log(good);
   const countPositiveFeedback = (total, good) => {
     const result = (parseInt(good) * 100) / total;
     return total ? parseInt(result.toFixed(0)) : 0;
   };
   const countTotalFeedback = () => {
+    const state = { good, bad, neutral };
     if (state) return Object.values(state).reduce((counter, value) => (counter += value), 0);
+    return 0;
   };
+
   const onLeaveFeedback = e => {
-    const option = e.target.name;
-    switch (option) {
+    switch (e.target.name) {
       case 'good':
-        setGood(prev => ++prev);
+        setGood(good ? good + 1 : 1);
         break;
       case 'neutral':
-        setNeutral(prev => ++prev);
+        setNeutral(neutral ? neutral + 1 : 1);
         break;
       case 'bad':
-        setBad(prev => ++prev);
+        setBad(bad ? bad + 1 : 1);
         break;
       default:
         return;
